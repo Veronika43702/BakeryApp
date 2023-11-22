20ea024dbb012f4bf1213fdb9a15722b74d2484f
Veronika Chekulaeva <nika.702@mail.ru>
2023-11-22 20:08:53 +0300

    menu to open tables

--- app/src/main/java/com/example/bakeryapp/activity/CupcakeFragment.kt
+++ app/src/main/java/com/example/bakeryapp/activity/CupcakeFragment.kt
@@ -17,9 +17,55 @@ class CupcakeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val binding = FragmentCupcakeBinding.inflate(layoutInflater, container, false)
-            binding.spongeMenu.setOnClickListener{
-                //sponge_ingredient_table
+
+        binding.spongeMenu.setOnClickListener{
+                if (binding.spongeMenu.isChecked) {
+                    binding.spongeIngredientTable.visibility = View.GONE
+                } else {
+                    binding.spongeIngredientTable.visibility = View.VISIBLE
+                }
             }
+
+        binding.fillingMenu.setOnClickListener{
+            if (binding.fillingMenu.isChecked) {
+                binding.fillingIngredientTable.visibility = View.GONE
+            } else {
+                binding.fillingIngredientTable.visibility = View.VISIBLE
+            }
+        }
+
+        binding.creamMenu.setOnClickListener{
+            if (binding.creamMenu.isChecked) {
+                binding.creamIngredientTable.visibility = View.GONE
+            } else {
+                binding.creamIngredientTable.visibility = View.VISIBLE
+            }
+        }
+
+        binding.spongeReceiptMenu.setOnClickListener{
+            if (binding.spongeReceiptMenu.isChecked) {
+                binding.spongeReceipt.visibility = View.GONE
+            } else {
+                binding.spongeReceipt.visibility = View.VISIBLE
+            }
+        }
+
+        binding.fillingReceiptMenu.setOnClickListener{
+            if (binding.fillingReceiptMenu.isChecked) {
+                binding.fillingReceipt.visibility = View.GONE
+            } else {
+                binding.fillingReceipt.visibility = View.VISIBLE
+            }
+        }
+
+        binding.creamReceiptMenu.setOnClickListener{
+            if (binding.creamReceiptMenu.isChecked) {
+                binding.creamReceipt.visibility = View.GONE
+            } else {
+                binding.creamReceipt.visibility = View.VISIBLE
+            }
+        }
+
         return binding.root
     }
 }
